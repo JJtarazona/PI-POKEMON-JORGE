1424f237493b376c38778966a10b81f9adb8caee
Jorge <116285852+JJtarazona@users.noreply.github.com>
2023-06-27 18:35:42 -0500

    BD Creada - Modelos Creados y server Levantado

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,10 +1,55 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
+  sequelize.define("pokemon", {
+    ID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    vida: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    ataque: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    defensa: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    velocidad: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    altura: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    peso: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+  });
+
+  sequelize.define("type", {
+    ID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
+    nombre: {
       type: DataTypes.STRING,
       allowNull: false,
     },
